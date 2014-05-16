537bbd667d6a3d73c499bb385e8e05520c165348
Ben Keen <ben.keen@gmail.com>
2014-05-15 20:29:57 -0700

    1.0.1

--- core_structure.php
+++ core_structure.php
@@ -2512,101 +2512,6 @@ $STRUCTURE["2.0.3"]["tables"]["module_export_types"] = array(
     "Default" => ""
   )
 );
-$STRUCTURE["2.0.3"]["tables"]["module_extended_client_fields"] = array(
-  array(
-    "Field"   => "client_field_id",
-    "Type"    => "mediumint(8) unsigned",
-    "Null"    => "NO",
-    "Key"     => "PRI",
-    "Default" => ""
-  ),
-  array(
-    "Field"   => "template_hook",
-    "Type"    => "varchar(255)",
-    "Null"    => "YES",
-    "Key"     => "",
-    "Default" => ""
-  ),
-  array(
-    "Field"   => "admin_only",
-    "Type"    => "enum('yes','no')",
-    "Null"    => "YES",
-    "Key"     => "",
-    "Default" => ""
-  ),
-  array(
-    "Field"   => "field_label",
-    "Type"    => "varchar(255)",
-    "Null"    => "NO",
-    "Key"     => "",
-    "Default" => ""
-  ),
-  array(
-    "Field"   => "field_type",
-    "Type"    => "enum('textbox','textarea','password','radios','checkboxes','select','multi-select')",
-    "Null"    => "NO",
-    "Key"     => "",
-    "Default" => ""
-  ),
-  array(
-    "Field"   => "field_orientation",
-    "Type"    => "enum('horizontal','vertical','na')",
-    "Null"    => "NO",
-    "Key"     => "",
-    "Default" => "na"
-  ),
-  array(
-    "Field"   => "default_value",
-    "Type"    => "varchar(255)",
-    "Null"    => "YES",
-    "Key"     => "",
-    "Default" => ""
-  ),
-  array(
-    "Field"   => "is_required",
-    "Type"    => "enum('yes','no')",
-    "Null"    => "YES",
-    "Key"     => "",
-    "Default" => ""
-  ),
-  array(
-    "Field"   => "error_string",
-    "Type"    => "mediumtext",
-    "Null"    => "YES",
-    "Key"     => "",
-    "Default" => ""
-  ),
-  array(
-    "Field"   => "field_order",
-    "Type"    => "smallint(6)",
-    "Null"    => "NO",
-    "Key"     => "",
-    "Default" => ""
-  )
-);
-$STRUCTURE["2.0.3"]["tables"]["module_extended_client_field_options"] = array(
-  array(
-    "Field"   => "client_field_id",
-    "Type"    => "mediumint(9)",
-    "Null"    => "NO",
-    "Key"     => "PRI",
-    "Default" => ""
-  ),
-  array(
-    "Field"   => "option_text",
-    "Type"    => "varchar(255)",
-    "Null"    => "YES",
-    "Key"     => "",
-    "Default" => ""
-  ),
-  array(
-    "Field"   => "field_order",
-    "Type"    => "smallint(6)",
-    "Null"    => "NO",
-    "Key"     => "PRI",
-    "Default" => ""
-  )
-);
 $STRUCTURE["2.0.3"]["tables"]["module_menu_items"] = array(
   array(
     "Field"   => "menu_id",
@@ -2997,7 +2902,7 @@ $STRUCTURE["2.0.3"]["tables"]["view_filters"] = array(
   ),
   array(
     "Field"   => "operator",
-    "Type"    => "enum('equals','not_equals','like','before','after')",
+    "Type"    => "enum('equals','not_equals','like','not_like','before','after')",
     "Null"    => "NO",
     "Key"     => "",
     "Default" => "equals"
